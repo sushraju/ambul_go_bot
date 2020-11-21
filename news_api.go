c98228df025573bc1a670fee1e8be5ecbe27906d
Suresh Raju <sushraju@gmail.com>
2020-11-21 12:52:23 -0800

    formatting

--- news_api.go
+++ news_api.go
@@ -56,7 +56,7 @@ func (na *NewsAPI) GetEverything() (*NewsArticles, error) {
 
 	time.LoadLocation("America/Los_Angeles")
 	var (
-    from     = time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+    	from     = time.Now().AddDate(0, 0, -1).Format("2006-01-02")
 		to       = time.Now().Format("2006-01-02")
 		lang     = "en"
 		sortBy   = "relevancy"
